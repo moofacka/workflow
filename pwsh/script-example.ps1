95d4ae78026109598a5d07af36501edbab8a185f
Mats Tegbjer <mats.tegbjer@gmail.com>
2024-05-16 10:38:18 +0200

    Update script-example.ps1

--- pwsh/script-example.ps1
+++ pwsh/script-example.ps1
@@ -1,1 +1,17 @@
-Write-Output "Script ran successfully"
+param(
+    [Switch]$Fail, 
+    [Switch]$ExitCode, 
+    $Parameter1
+)
+
+if ($Fail) {
+    throw "This script fails!"
+}
+
+if ($ExitCode) {
+    exit 5
+}
+
+$Env:MyVariable
+$Parameter1
+$PSVersionTable
